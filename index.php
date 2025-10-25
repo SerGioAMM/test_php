41675bf6f281dbc6809954fcd0c87e3b448fc0ba
Sergio Murcia <sergimurcia01@gmail.com>
2025-10-25 11:01:06 -0600

    a

--- index.php
+++ index.php
@@ -122,7 +122,7 @@
             ⚠️ SOLO PARA LABORATORIO - CONTIENE VULNERABILIDADES INTENCIONALES
         </div>
         <?pp
-        <div class="content" error 1>
+        <div class="content" error 2>
             <div class="vuln-info">
                 <h3>🐛 Vulnerabilidades Implementadas:</h3>
                 <ul>
